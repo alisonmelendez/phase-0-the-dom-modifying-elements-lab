71d3ac8e824459a2f45cada4915f79d65ff89a97
Alison Melendez <alisonmelendez132@gmail.com>
2021-12-29 21:56:12 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+const yourName = 'Alison';
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = `${yourName} is the champion`
+document.body.append(newHeader);
